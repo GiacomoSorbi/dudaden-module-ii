fee5fd49eb2551cf5c52de8891bfe4d26826a62a
Denis <denissdudarevs@gmail.com>
2021-01-11 21:10:33 +0000

    Heroku deployment

---
+++ build/precache-manifest.06fb4c6c29ab8181d316b8f528e8caf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46e5df5648ef51f6ab701a9ec1b57d36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa1c0ee7ff039d8afcd",
+    "url": "/static/css/main.2e118f7f.chunk.css"
+  },
+  {
+    "revision": "99c2b4f9ba5c54b74303",
+    "url": "/static/js/2.6a164ef7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6a164ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa1c0ee7ff039d8afcd",
+    "url": "/static/js/main.b31c3d53.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// const port = process.env.PORT || 3000;
+//   console.log(port);
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
