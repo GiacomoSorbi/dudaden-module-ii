41d043cf069993cfcf1ba2663a2ee4ca3bc83135
Denis <denissdudarevs@gmail.com>
2021-01-24 13:21:26 +0000

    Updates

---
+++ precache-manifest.efe50b47b6044b92438bf1c8467814a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807fa013d701c931d88ee543de80f98a",
+    "url": "/dudaden-module-ii/index.html"
+  },
+  {
+    "revision": "dc78ea3dfd77059c26d5",
+    "url": "/dudaden-module-ii/static/css/main.b357d44a.chunk.css"
+  },
+  {
+    "revision": "5bb3257ab17062fc9d6a",
+    "url": "/dudaden-module-ii/static/js/2.cabe7175.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/dudaden-module-ii/static/js/2.cabe7175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc78ea3dfd77059c26d5",
+    "url": "/dudaden-module-ii/static/js/main.7ed8cb51.chunk.js"
+  },
+  {
+    "revision": "536c57d45a04590bf40b",
+    "url": "/dudaden-module-ii/static/js/runtime-main.93ff3732.js"
+  }
+]);
